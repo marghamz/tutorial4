df2a74aa709aea86702f5d53afa82af73995e87f
Margaret Hamilton <marghamz@gmail.com>
2019-08-21 20:16:27 +1200

    added subtraction test

--- src/test/java/tut4p/CalcTest.java
+++ src/test/java/tut4p/CalcTest.java
@@ -9,4 +9,9 @@ public class CalcTest {
 	public void testAdd() {
 		assertEquals(6, new Calc().add());
 	}
+	
+	@Test
+	public void testSubtract() {
+		assertEquals(2, new Calc().subtract());
+	}
 }
